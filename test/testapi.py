2cf59396fd17c22a469c547922068bbcf7145482
Elton Chou <plscd748@gmail.com>
2019-01-23 12:04:50 +0800

    xivapi initialize

--- test/testapi.py
+++ test/testapi.py
@@ -5,10 +5,10 @@ from EorzeaEnv.EorzeaApi import Xivapi
 
 class TestApi (unittest.TestCase):
     def setUp(self):
-        self.xivapi = Xivapi.Client(os.getenv('XIV_API_KEY'))
+        self.Xivapi = Xivapi.Client(os.getenv('XIV_API_KEY'))
 
     def test_api(self):
-        r = self.xivapi.content()
+        r = self.Xivapi.content()
         self.assertEqual(r.status_code, 200)
 
 if __name__ == "__main__":
